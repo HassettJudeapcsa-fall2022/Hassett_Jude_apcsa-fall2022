370dbed0aa96c44aa3142ec7d1e31c30167cc7df
JudeHassett <hassettj2043@my.sduhsd.net>
2022-08-17 11:41:51 -0700

    Finished AsciiArt and AsciiBox assignments

--- Unit-00/src/AsciiArt.java
+++ Unit-00/src/AsciiArt.java
@@ -10,14 +10,22 @@ public class AsciiArt
 {
 	public static void main ( String[] args )
 	{
-		System.out.println("Your Name \n\n" );
-		System.out.println("What type of ANIMAL YOU WILL DRAW" );
-		System.out.println("\n\n\n\n" );
+		System.out.println("Jude Hassett \n\n" );
+		System.out.println("Cat" );
+		System.out.println("\n\n" );
 
-		System.out.println("                /\\                " );
-		System.out.println("               /  \\               " );
-		System.out.println("              /    \\              " );
-		System.out.println("             [------]             " );
+		System.out.println("                            __");
+		System.out.println("                           / /");
+		System.out.println("      /\\__/\\              / /");
+		System.out.println("     / O  O \\            / /");
+		System.out.println("    |   w    |__________/ /");
+		System.out.println("     \\______/             |");
+		System.out.println("          |               |");
+		System.out.println("          \\_   _________  |");
+		System.out.println("           || |        || |");
+		System.out.println("           || |        || |");
+		System.out.println("           || |        || |");
+		System.out.println("          /_/_|       /_/_|");
 		//add other output
 
 		System.out.println(" \n\n\n\nHelpFul Hints" );
--- Unit-00/src/AsciiBox.java
+++ Unit-00/src/AsciiBox.java
@@ -10,8 +10,17 @@ public class AsciiBox
 {
 	public static void main(String[] args)
 	{
-		System.out.println("name \t  date \n\n" );
-		System.out.println("+++++++++++++++++++++++++ " );
-
+		System.out.println("name Jude Hassett  date 8/17/2022 \n\n" );
+		System.out.println("-------------------------");
+		System.out.println("|                       |"); 
+		System.out.println("|                       |");
+		System.out.println("|                       |"); 
+		System.out.println("|                       |");
+		System.out.println("|                       |"); 
+		System.out.println("|                       |");
+		System.out.println("|                       |"); 
+		System.out.println("|                       |");
+		System.out.println("|                       |"); 
+		System.out.println("-------------------------");
 	}
 }
