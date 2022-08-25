de4a7942cbf879806ff0eabd3e0fb07a6298170c
JudeHassett <hassettj2043@my.sduhsd.net>
2022-08-25 11:17:42 -0700

    Uploaded Unit-05 and finished AvgLen assignment

---
+++ Unit-05/src/FirstLastVowel.java
@@ -0,0 +1,17 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+
+public class FirstLastVowel
+{
+   public static String go( String a )
+	{
+	   char first = a.charAt(0);
+	   char last = a.charAt(a.length()-1);
+	   String vowels = "aeiouAEIOU";
+	   
+		return "no";
+	}
+}
