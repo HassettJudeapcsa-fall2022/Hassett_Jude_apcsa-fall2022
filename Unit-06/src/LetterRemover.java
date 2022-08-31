c40373f5ef931bf33d777adb210b378361e6c866
JudeHassett <hassettj2043@my.sduhsd.net>
2022-08-31 12:19:25 -0700

    completed letterremover assignment

--- Unit-06/src/LetterRemover.java
+++ Unit-06/src/LetterRemover.java
@@ -7,35 +7,34 @@ import static java.lang.System.*;
 public class LetterRemover
 {
    private String sentence;
-   private char lookFor;
 
 	public LetterRemover()
 	{
+		setRemover("");
 	}
 
 	//add in second constructor
-	public LetterRemover(String s, char rem) {
-		setRemover(s,rem);
+	public LetterRemover(String s) {
+		setRemover(s);
 	}
 	
-	public void setRemover(String s, char rem)
+	public void setRemover(String s)
 	{
 		sentence = s;
-		
-		lookFor = rem;
 	}
 
 	public String removeLetters()
 	{
 		String cleaned=sentence;
-		for(int i= sentence.length(); i==0; i--){
-			
+		for(int i = 0; i<sentence.length(); i++){
+			cleaned = sentence.substring(i);
+			out.println(cleaned);
 		}
 		return cleaned;
 	}
 
 	public String toString()
 	{
-		return sentence + " - letter to remove " + lookFor;
+		return removeLetters();
 	}
 }
--- Unit-06/src/LetterRemoverRunner.java
+++ Unit-06/src/LetterRemoverRunner.java
@@ -3,12 +3,16 @@
 //Name -
 
 import static java.lang.System.*;
+import java.util.*;
 
 public class LetterRemoverRunner
 {
 	public static void main( String args[] )
 	{
-		//add test cases		
-											
+		Scanner key = new Scanner(System.in);
+		out.print("input a word :: ");
+		String triangle = key.next();
+		LetterRemover test = new LetterRemover(triangle);
+		test.removeLetters();
 	}
 }
