9945cdb522231171a6c1d13ceba98f9fbcab7be7
JudeHassett <hassettj2043@my.sduhsd.net>
2022-09-15 11:18:11 -0700

    worked on rayoddtoeven

--- Unit-08/src/RayOddToEven.java
+++ Unit-08/src/RayOddToEven.java
@@ -3,10 +3,31 @@
 //Name -
 //Date -
 
+
+
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	public static int go(int[] ray)
 	{
-		return false;
+		int oddindex = -1;
+		int length = 0;
+		for(int i=0; i<ray.length; i++) {
+			if(ray[i]%2 == 1) {
+				oddindex = i;
+				break;
+			}
+		}
+		if(oddindex == -1) return oddindex;
+		for(int i=oddindex+1; i<ray.length; i++) {
+			length++;
+			if(ray[i]%2 == 0) {
+				break;
+			}
+		}
+		System.out.print(length + " " + oddindex +1 + ray.length);
+		if(length == ray.length-oddindex+1) {
+			return -1;
+		}
+		return length;
 	}
 }
