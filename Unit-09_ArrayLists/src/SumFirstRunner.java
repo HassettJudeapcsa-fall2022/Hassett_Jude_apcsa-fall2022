3e3d5e9f6a4ef8fd6442b4bb310b46c25af6e2b0
JudeHassett <111383050+HassettJudeapcsa-fall2022@users.noreply.github.com>
2022-09-29 22:50:58 -0700

    finished listsumfirst

--- Unit-09_ArrayLists/src/SumFirstRunner.java
+++ Unit-09_ArrayLists/src/SumFirstRunner.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
@@ -6,6 +10,15 @@
 public class SumFirstRunner
 {
 	public static void main( String args[] )
-	{										
+	{
+		List<Integer> a;
+		a = new ArrayList<Integer>(Arrays.asList(-99,1,2,3,4,5,6,7,8,9,10,5));
+		System.out.println(ListSumFirst.go(a));
+		List<Integer> b;
+		b = new ArrayList<Integer>(Arrays.asList(10,9,8,7,6,5,4,3,2,1,-99));
+		System.out.println(ListSumFirst.go(b));
+		List<Integer> c;
+		c = new ArrayList<Integer>(Arrays.asList(10,20,30,40,50,-11818,40,30,20,10));
+		System.out.println(ListSumFirst.go(c));
 	}
 }
