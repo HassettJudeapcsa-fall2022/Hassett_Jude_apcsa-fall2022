08765d3be8bdd2b46399677064202f8659b2082a
JudeHassett <111383050+HassettJudeapcsa-fall2022@users.noreply.github.com>
2022-09-29 22:23:49 -0700

    finished oddtoeven

--- Unit-09_ArrayLists/src/OddToEvenRunner.java
+++ Unit-09_ArrayLists/src/OddToEvenRunner.java
@@ -13,5 +13,12 @@ public class OddToEvenRunner
 	{
 		List<Integer> a;
 		a = new ArrayList<Integer>(Arrays.asList(7,1,5,3,11,5,6,7,8,9,10,12345,11));
+		System.out.println(ListOddToEven.go(a));
+		List<Integer> b;
+		b = new ArrayList<Integer>(Arrays.asList(11,9,8,7,6,5,4,3,2,1,-99,7));
+		System.out.println(ListOddToEven.go(b));
+		List<Integer> c;
+		c = new ArrayList<Integer>(Arrays.asList(2,4,6,8,8));
+		System.out.println(ListOddToEven.go(c));
 	}
 }
