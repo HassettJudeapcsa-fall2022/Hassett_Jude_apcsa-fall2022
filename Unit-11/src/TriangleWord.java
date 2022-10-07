25b9ec23f66af20b37c026addf90796ebf60c582
JudeHassett <hassettj2043@my.sduhsd.net>
2022-10-07 11:03:26 -0700

    finished trianglewordrunner

--- Unit-11/src/TriangleWord.java
+++ Unit-11/src/TriangleWord.java
@@ -11,5 +11,9 @@ public class TriangleWord
 		
 	public static void printTriangle(String word)
 	{
+		for (int i = 0; i < word.length()+1; i++) {
+			for(int x = 0; x < i; x++) System.out.print(word.substring(0,i));
+			System.out.println();
+		}
 	}
 }
--- Unit-11/src/TriangleWordRunner.java
+++ Unit-11/src/TriangleWordRunner.java
@@ -9,6 +9,15 @@ public class TriangleWordRunner
 {
 	public static void main( String args[] )
 	{
-		
+		Scanner key = new Scanner(System.in);
+		TriangleWord test = new TriangleWord();
+		char confirm = 'y';
+		do {
+			System.out.println("Enter the word :: ");
+			String word = key.next();
+			test.printTriangle(word);
+			System.out.println("\nWould you like to go again? (y/n) :: ");
+			confirm = key.next().charAt(0);
+		} while(confirm == 'y');
 	}
 }
