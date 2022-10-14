f0e4b1d72e4910d4fad3503b2fa498311c0bc7ac
JudeHassett <hassettj2043@my.sduhsd.net>
2022-10-14 10:14:36 -0700

    completed monster lab

--- Unit-12/src/MonsterRunner.java
+++ Unit-12/src/MonsterRunner.java
@@ -12,11 +12,24 @@ public class MonsterRunner
 		Scanner keyboard = new Scanner(System.in);
 		
 		//ask for name and size
-		
+		System.out.print("Input monster 1's name :: ");
+		String nm = keyboard.next();
+		System.out.print("Input monster 1's size :: ");
+		int sz = keyboard.nextInt();
 		//instantiate monster one
-		
+		Monster mon1 = new Skeleton(nm, sz);
 		//ask for name and size
-		
+		System.out.print("Input monster 2's name :: ");
+		nm = keyboard.next();
+		System.out.print("Input monster 2's size :: ");
+		sz = keyboard.nextInt();
 		//instantiate monster two
+		Monster mon2 = new Skeleton(nm,sz);
+		
+		if(mon1.isBigger(mon2)) System.out.println("Monster One is bigger than Monster Two");
+		else System.out.println("Monster One is smaller than Monster Two");
+		
+		if(mon1.namesTheSame(mon2))System.out.println("Monster One has the same name as Monster Two");
+		else System.out.println("Monster One does not have the same name as Monester Two");
 	}
 }
--- Unit-12/src/Skeleton.java
+++ Unit-12/src/Skeleton.java
@@ -7,10 +7,53 @@ import static java.lang.System.*;
 public class Skeleton implements Monster
 {
 	//add instance variables	
+	private String name;
+	private int size;
 
 	//add a constructor
+	public Skeleton() {
+		this("",0);
+	}
+	
+	public Skeleton(String nm,int sz) {
+		setName(nm);
+		setSize(sz);
+	}
+	
+	public void setName(String nm) {
+		name = nm;
+	}
 
+	public void setSize(int sz) {
+		size = sz;
+	}
+	
 	//add code to implement the Monster interface
-
+	public int getHowBig() {
+		return size;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public boolean isBigger(Monster other) {
+		if(other.getHowBig()<size) return false;
+		return true;
+	}
+	
+	public boolean isSmaller(Monster other) {
+		if(other.getHowBig()>size) return false;
+		return true;
+	}
+	
+	public boolean namesTheSame(Monster other) {
+		if(other.getName().equals(name)) return true;
+		return false;
+	}
+	
 	//add a toString
+	public String toString() {
+		return name + " " + size;
+	}
 }
