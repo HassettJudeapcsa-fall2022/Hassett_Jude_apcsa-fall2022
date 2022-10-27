6b45b63437dbc4d7b958a6cd69f81ba84f495113
JudeHassett <hassettj2043@my.sduhsd.net>
2022-10-27 10:45:31 -0700

    finished recursion one

--- Unit-14/src/Recursion234Runner.java
+++ Unit-14/src/Recursion234Runner.java
@@ -8,8 +8,11 @@ public class Recursion234Runner
 {
 	public static void main(String args[])
 	{
-		System.out.println(RecursionFunOne.countOddDigits(4532));
-
+		System.out.println(RecursionFunOne.countEvenDigits(4532));
+		System.out.println(RecursionFunOne.countEvenDigits(11145322));
+		System.out.println(RecursionFunOne.countEvenDigits(224532714));
+		System.out.println(RecursionFunOne.countEvenDigits(2468));
+		System.out.println(RecursionFunOne.countEvenDigits(13579));
 		//add more test cases
 	}
 }
--- Unit-14/src/RecursionFunOne.java
+++ Unit-14/src/RecursionFunOne.java
@@ -6,10 +6,14 @@ import static java.lang.System.*;
 
 public class RecursionFunOne
 {
-	public static int countOddDigits(int num)
+	public static int countEvenDigits(int num)
 	{
-
-
-		return 0;
+		if(num == 0) {
+			return 0;
+		}
+		if((num % 10)%2 == 0) {
+			return 1 + countEvenDigits(num/10);
+		}
+		return countEvenDigits(num/10);
 	}
 }
