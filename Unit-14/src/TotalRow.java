def8db4ecf6cc32fe28bb32b585647f5c11e2356
JudeHassett <hassettj2043@my.sduhsd.net>
2022-10-28 11:27:59 -0700

    completed totalRow lab

--- Unit-14/src/TotalRow.java
+++ Unit-14/src/TotalRow.java
@@ -8,7 +8,15 @@ public class TotalRow
 {
     public static List<Integer> getRowTotals( int[][] m )
     {
-    	//add code here
-		return null;
+    	List<Integer> Arr = new ArrayList<>();
+    	int sum = 0;
+    	for(int r = 0; r < m.length; r++) {
+    		for(int c = 0; c < m[r].length; c++) {
+    			sum += m[r][c];
+    		}
+    		Arr.add(sum);
+    		sum = 0;
+    	}
+		return Arr;
     }
 }
