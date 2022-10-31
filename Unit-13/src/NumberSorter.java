49a7a1acbad463aeef7e32c46af8825feb1ad97e
JudeHassett <hassettj2043@my.sduhsd.net>
2022-10-31 11:32:49 -0700

    completed numbersorter

--- Unit-13/src/NumberSorter.java
+++ Unit-13/src/NumberSorter.java
@@ -14,13 +14,29 @@ public class NumberSorter
 
 	private static int getNumDigits(int number)
 	{
-		int count = 0;
-		return count;
+		if(number == 0) {
+			return 0;
+		}
+		return 1 + getNumDigits(number/10);
 	}
 
 	public static int[] getSortedDigitArray(int number)
 	{
-		int[] sorted = null;
+		int[] sorted = new int[getNumDigits(number)];
+		for(int i = 0; i < sorted.length; i++) {
+			sorted[i] = number%10;
+			number/=10;
+		}
+		for(int i = 0; i < sorted.length; i++) {
+			for(int x = i; x < sorted.length; x++) {
+				int temp = 0;
+				if(sorted[x] < sorted[i]) {
+					temp = sorted[x];
+					sorted[x] = sorted[i];
+					sorted[i] = temp;
+				}
+			}
+		}
 		return sorted;
 	}
 }
