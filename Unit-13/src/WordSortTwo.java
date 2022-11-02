efb797f4a6087b06d760e288d56c9e3958575595
JudeHassett <hassettj2043@my.sduhsd.net>
2022-11-02 12:08:11 -0700

    completed wordsorttwo

--- Unit-13/src/WordSortTwo.java
+++ Unit-13/src/WordSortTwo.java
@@ -12,15 +12,30 @@ public class WordSortTwo
 
 	public WordSortTwo(String sentence)
 	{
+		wordRay = sentence.split(" ");
+		sort();
 	}
 
 	public void sort()
 	{
+		for(int i = 0; i < wordRay.length; i++) {
+			for(int x = i; x < wordRay.length; x++) {
+				if(wordRay[i].compareTo(wordRay[x]) > 0) {
+					String temp = wordRay[i];
+					wordRay[i] = wordRay[x];
+					wordRay[x] = temp;
+				}
+			}
+		}
 	}
 
 	public String toString()
 	{
 		String output="";
+		for(int i = 0; i < wordRay.length; i++) {
+			output+= wordRay[i];
+			output+= "\n";
+		}
 		return output+"\n\n";
 	}
 }
--- Unit-13/src/WordSortTwoRunner.java
+++ Unit-13/src/WordSortTwoRunner.java
@@ -19,6 +19,8 @@ public class WordSortTwoRunner
 		{
 			String sentence = file.nextLine();
 			//instantiate a new WordSort
+			WordSortTwo test = new WordSortTwo(sentence);
+			System.out.print(test);
 		}
 	}
 }
